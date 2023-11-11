700672c24847edc5b814b8a0881a69d928ed50f3
Akash Kumar <akash.kumar@fau.de>
2023-11-11 17:28:11 +0100

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DSSS_homework_2',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
